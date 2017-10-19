ecd7e3a2ad45b18ae55362377206d685517b5530
VitorCarminatti <vitor.carminatti@hotmail.com>
2017-10-19 16:56:10 -0200

    Ajsutes servidor de email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  Rails.application.routes.default_url_options[:host] = 'pedidoselecao.herokuapp.com'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -101,7 +101,6 @@ Rails.application.configure do
   :path => '/:class/:attachment/:id_partition/:style/:filename',
 }
   config.action_mailer.default_url_options = { :host => 'pedidoselecao.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'pedidoselecao.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
