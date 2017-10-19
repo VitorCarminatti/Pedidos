87a19a7378cb3ee5544295ac3c8d8952c3d44ead
VitorCarminatti <vitor.carminatti@hotmail.com>
2017-10-19 16:04:18 -0200

    Ajuste envio de e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,15 +56,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.wonder.com.br",
-    tls: true,
-    port: "465",
-    domain: ENV["WONDER_DOMAIN"],
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: ENV["GMAIL_DOMAIN"],
     authentication: :plain,
     enable_starttls_auto: true,
-    user_name: ENV["WONDER_USERNAME"],
-    password: ENV["WONDER_PASSWORD"],
-    openssl_verify_mode: 'none'
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
   }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,14 +108,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.wonder.com.br",
-    tls: true,
-    port: "465",
-    domain: ENV["WONDER_DOMAIN"],
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: ENV["GMAIL_DOMAIN"],
     authentication: :plain,
     enable_starttls_auto: true,
-    user_name: ENV["WONDER_USERNAME"],
-    password: ENV["WONDER_PASSWORD"],
-    openssl_verify_mode: 'none'
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
   }
 end
